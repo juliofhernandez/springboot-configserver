b13f997440c50ceda56b887f54ad12fab70a230e
juliofhernandez <juliofernando1c@gmail.com>
2024-12-29 19:17:36 +0100

    Init configServer

---
+++ src/test/java/org/example/springbootservicioconfigserver/SpringbootServicioConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootservicioconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
